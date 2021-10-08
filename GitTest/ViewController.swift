869a3d842417074a9518924e981350e429385e64
Candice_CHEN <a404879018@gmail.com>
2021-10-09 00:03:56 +0800

    Initial Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by 陈思玫 on 2021/10/9.
+//  Copyright © 2021 陈思玫. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
